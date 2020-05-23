33633289bd07b8375bdaa00cbea5e8af706cc80b
William Schwindt <williammschwindt@gmail.com>
2020-05-22 17:27:07 -0700

    users integration tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -14,7 +14,9 @@ afterAll(async () => {
 
 describe('auth integration tests', () => {
     test('POST register', async () => {
-        const res = await supertest(server).post('/api/auth/register').send({
+        const res = await supertest(server)
+        .post('/api/auth/register')
+        .send({
             username: 'test',
             password: '123',
             name: 'testing',
@@ -24,7 +26,9 @@ describe('auth integration tests', () => {
     })
 
     test('POST register (400)', async () => {
-        const res = await supertest(server).post('/api/auth/register').send({
+        const res = await supertest(server)
+        .post('/api/auth/register')
+        .send({
             username: 'test',
             password: '123',
             name: 'testing'
@@ -33,7 +37,9 @@ describe('auth integration tests', () => {
     })
 
     test('POST login', async () => {
-        const res = await supertest(server).post('/api/auth/login').send({
+        const res = await supertest(server)
+        .post('/api/auth/login')
+        .send({
             username: 'johndoe1',
             password: '123'
         })
@@ -41,7 +47,9 @@ describe('auth integration tests', () => {
     })
 
     test('POST login (wrong credentials)', async () => {
-        const res = await supertest(server).post('/api/auth/login').send({
+        const res = await supertest(server)
+        .post('/api/auth/login')
+        .send({
             username: 'john',
             password: '123'
         })
@@ -49,3 +57,65 @@ describe('auth integration tests', () => {
     })
 })
 
+describe('user integration tests', () => {
+    test('GET users', async () => {
+        const res = await supertest(server)
+        .get('/api/users')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test('GET useer', async () => {
+        const res = await supertest(server)
+        .get('/api/users/1')
+        expect(res.statusCode).toBe(200)
+        expect(res.body.username).toBe('johndoe1')
+    })
+
+    test('PUT user', async () => {
+        const res1 = await supertest(server)
+        .post('/api/auth/login')
+        .send({
+            username: 'johndoe1',
+            password: '123'
+        })
+
+        const res2 = await supertest(server)
+        .put('/api/users/1')
+        .set('authorization', res1.body.token)
+        .send({
+            name: 'changed'
+        })
+
+        expect(res2.body.name).toBe('changed') 
+    })
+
+    test('PUT user (unauthorized)', async () => {
+        const res = await supertest(server)
+        .put('/api/users/1')
+        .send({
+            name: 'changed'
+        })
+
+        expect(res.statusCode).toBe(401)
+    })
+
+    test('DELETE user', async () => {
+        const res1 = await supertest(server)
+        .post('/api/auth/login')
+        .send({
+            username: 'johndoe1',
+            password: '123'
+        })
+
+        const res2 = await supertest(server)
+        .delete('/api/users/1')
+        .set('authorization', res1.body.token)
+        expect(res2.statusCode).toBe(204)
+    })
+
+    test('DELETE user (unauthorized)', async () => {
+        const res = await supertest(server)
+        .delete('/api/users/1')
+        expect(res.statusCode).toBe(401)
+    })
+})
