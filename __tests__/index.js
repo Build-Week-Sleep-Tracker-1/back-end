1189ec7863321c63cf51a7e2ee10e9cfd6bb8d3e
William Schwindt <williammschwindt@gmail.com>
2020-05-22 17:37:30 -0700

    entries integration tests and database cleaned out

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,6 @@
 const supertest = require('supertest')
 const server = require('../index')
 const db = require('../data/config')
-const usersModel = require('../models/users-model')
-const entriesModel = require('../models/entries-model')
 
 beforeEach(async () => {
     await db.seed.run()
@@ -117,5 +115,83 @@ describe('user integration tests', () => {
         const res = await supertest(server)
         .delete('/api/users/1')
         expect(res.statusCode).toBe(401)
+    })
+})
+
+describe('user entries integration tests', () => {
+    test('GET user entries', async () => {
+        const res1 = await supertest(server)
+        .post('/api/auth/login')
+        .send({
+            username: 'johndoe1',
+            password: '123'
+        })
+
+        const res2 = await supertest(server)
+        .get('/api/users/1/entries')
+        .set('authorization', res1.body.token)
+
+        expect(res2.statusCode).toBe(200)
+    })
+
+    test('GET user entries (unauthorized)', async () => {
+        const res = await supertest(server)
+        .get('/api/users/1/entries')
+
+        expect(res.statusCode).toBe(401)
+    })
+
+    test('POST user entry', async () => {
+        const res1 = await supertest(server)
+        .post('/api/auth/login')
+        .send({
+            username: 'johndoe1',
+            password: '123'
+        })
+
+        const res2 = await supertest(server)
+        .post('/api/users/1/entries')
+        .set('authorization', res1.body.token)
+        .send({
+            date: '01-01-2020',
+            sleep_start: '11pm',
+            sleep_end: '7am',
+            total_time: 8,
+            mood_score: 4
+        })
+
+        expect(res2.body.date).toBe('01-01-2020') 
+    })
+
+    test('PUT user entry', async () => {
+        const res1 = await supertest(server)
+        .post('/api/auth/login')
+        .send({
+            username: 'johndoe1',
+            password: '123'
+        })
+
+        const res2 = await supertest(server)
+        .put('/api/users/1/entries/1')
+        .set('authorization', res1.body.token)
+        .send({
+            date: '01-04-2020'
+        })
+        expect(res2.body.date).toBe('01-04-2020')
+    })
+
+    test('DELETE user entry', async () => {
+        const res1 = await supertest(server)
+        .post('/api/auth/login')
+        .send({
+            username: 'johndoe1',
+            password: '123'
+        })
+
+        const res2 = await supertest(server)
+        .delete('/api/users/1/entries/1')
+        .set('authorization', res1.body.token)
+
+        expect(res2.statusCode).toBe(204)
     })
 })
